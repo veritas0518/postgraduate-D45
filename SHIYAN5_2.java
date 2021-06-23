977d776573d3dadef0dbbbc381c728bcc4e21e1d
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-23 09:22:32 +0800

    Read the contents of the file display on screen
    【问题描述】从指定文本文件test.txt中读取内容，并显示到屏幕上。
    【输入形式】当前目录下的文本文件 test.txt ，内容可能如下：
    
    test.txt.png
    
    【输出形式】屏幕上输出 test.txt 文件的内容

---
+++ SHIYAN5_2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+public class SHIYAN5_2 {
+    public static void main(String[] args) {
+        FileReader f;
+        try{
+             f= new FileReader("test.txt");
+             int num =0;
+
+             char b;
+
+
+            while((num=f.read())!=-1){
+
+            b=(char)num;
+
+            System.out.printf("%c",num);
+
+
+       }
+            f.close();
+
+        }
+catch(FileNotFoundException e){
+            System.out.println("not found file!");
+
+        }catch(Exception e){
+
+        }
+    }
+}
+
